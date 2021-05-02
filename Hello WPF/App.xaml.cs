695662d738abc0f921993e4026209cc40c2025bc
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-05-02 13:23:33 +0300

    Created button with all three types of routed events.

--- Hello WPF/App.xaml.cs
+++ Hello WPF/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace Hello_WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
--- Hello WPF/MainWindow.xaml.cs
+++ Hello WPF/MainWindow.xaml.cs
@@ -15,14 +15,26 @@ using System.Windows.Shapes;
 
 namespace Hello_WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thanx for clicking.");
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Clicked");
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("preview");
         }
     }
 }
