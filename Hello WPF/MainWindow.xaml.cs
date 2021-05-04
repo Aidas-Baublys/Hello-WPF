7c93c8dc29e57bf53554cb06f3869c26ccf73def
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-05-04 16:18:47 +0300

    Created ComboBox.

--- Hello WPF/MainWindow.xaml.cs
+++ Hello WPF/MainWindow.xaml.cs
@@ -20,26 +20,8 @@ namespace Hello_WPF
         public MainWindow()
         {
             InitializeComponent();
-
-            List<Match> matches = new List<Match>();
-            matches.Add(new Match() { Team1 = "Naciai", Team2 = "Duchai", Score1 = 4, Score2 = 2, Progress = 77 });
-            matches.Add(new Match() { Team1 = "Bybiai", Team2 = "Pizdukai", Score1 = 1, Score2 = 1, Progress = 23 });
-            matches.Add(new Match() { Team1 = "Daunai", Team2 = "Autistai", Score1 = 0, Score2 = 0, Progress = 98 });
-            matches.Add(new Match() { Team1 = "Ožiai", Team2 = "Avinai", Score1 = 2, Score2 = 1, Progress = 50 });
-
-            Matches.ItemsSource = matches;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (Matches.SelectedItem != null)
-            {
-                MessageBox.Show("Ojojoi kokios rungtinės, pizė.");
-            }
-            else
-            {
-                MessageBox.Show("Pasirink rungtinės kokias nors, pidare.");
-            }
+            
+            ColorComboBox.ItemsSource = typeof(Colors).GetProperties();
         }
     }
 }
